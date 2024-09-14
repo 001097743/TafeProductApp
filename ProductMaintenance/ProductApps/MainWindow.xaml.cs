7766aae4a1415a6e7f21579b4f39db7a34923f3c
001097743 <Qian.ZHONG@student.tafesa.edu.au>
2024-09-14 11:11:13 +0930

    Commit the branch feat_GST1.2

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -36,6 +36,7 @@ namespace ProductApps
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
                 totalChargeTextBox.Text = Convert.ToString(cProduct.TotalPayment + 25);
                 totalWrapTextBox.Text = Convert.ToString(cProduct.TotalPayment + 25 + 5);
+                totalGstTextBox.Text = Convert.ToString(Convert.ToDouble(cProduct.TotalPayment + 25 + 5)*1.1);
             }
             catch (FormatException)
             {
@@ -51,6 +52,7 @@ namespace ProductApps
             totalPaymentTextBlock.Text = "";
             totalChargeTextBox.Text = "";
             totalWrapTextBox.Text = "";
+            totalGstTextBox.Text = "";
         }
 
         private void closeButton_Click(object sender, RoutedEventArgs e)
